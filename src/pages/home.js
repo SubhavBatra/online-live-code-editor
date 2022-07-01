a62f9fea7c58ea2c6c473372480b892b94a6eb79
SubhavBatra <sbatra_be20@thapar.edu>
2022-07-02 01:51:51 +0530

    changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,10 +3,9 @@ import { v4 as uuidV4 } from 'uuid';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
-
-
 const Home = () => {
     const navigate = useNavigate();
+
     const [roomId, setRoomId] = useState('');
     const [username, setUsername] = useState('');
     const createNewRoom = (e) => {
@@ -14,8 +13,8 @@ const Home = () => {
         const id = uuidV4();
         setRoomId(id);
         toast.success('Created a new room');
-
     };
+
     const joinRoom = () => {
         if (!roomId || !username) {
             toast.error('ROOM ID & username is required');
@@ -29,16 +28,20 @@ const Home = () => {
             },
         });
     };
+
     const handleInputEnter = (e) => {
         if (e.code === 'Enter') {
             joinRoom();
         }
     };
-
     return (
         <div className="homePageWrapper">
             <div className="formWrapper">
-                <h3>🔴 LIVE Code Editor</h3>
+                <img
+                    className="homePageLogo"
+                    src="/code-sync.png"
+                    alt="code-sync-logo"
+                />
                 <h4 className="mainLabel">Paste invitation ROOM ID</h4>
                 <div className="inputGroup">
                     <input
@@ -48,7 +51,7 @@ const Home = () => {
                         onChange={(e) => setRoomId(e.target.value)}
                         value={roomId}
                         onKeyUp={handleInputEnter}
-                     />
+                    />
                     <input
                         type="text"
                         className="inputBox"
@@ -62,23 +65,24 @@ const Home = () => {
                     </button>
                     <span className="createInfo">
                         If you don't have an invite then create &nbsp;
-                        <a onClick={createNewRoom} href="" className="createNewBtn">
+                        <a
+                            onClick={createNewRoom}
+                            href=""
+                            className="createNewBtn"
+                        >
                             new room
                         </a>
                     </span>
-                    </div>
+                </div>
             </div>
             <footer>
                 <h4>
                     Built with 💛 &nbsp; by &nbsp;
-                    <a href="https://github.com/SubhavBatra">Subhav Batra</a>
+                    <a href="https://github.com/codersgyan">Coder's Gyan</a>
                 </h4>
             </footer>
         </div>
     );
-
-    
 };
 
 export default Home;
-
